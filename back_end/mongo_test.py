fdf528eb73414315cc812007228abf98713202ce
louis-pierre <louispierre.gavaud@gmail.com>
2019-12-01 15:21:40 +0100

    Add of docker volume for db

--- back_end/mongo_test.py
+++ back_end/mongo_test.py
@@ -14,7 +14,7 @@ class mongo(Resource):
         #second mongodb must match the docker-compose file name
         client = MongoClient("mongodb://mongodb:27017")
 
-        ##WARNING IF data was not persisted, after creating the container, db1 and collecction people need to be created !
+        ##WARNING IF data was not persisted (with docker volumes), after creating the container, db1 and collection "people" need to be created firsr !
         db = client.db1
         collection = db.people
         return list(collection.find({},{"_id":0}))
